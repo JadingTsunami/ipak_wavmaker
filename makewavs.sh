efccbbbf3b39be44ed36625159dd7cbc964a749a
JadingTsunami <JadingTsunami@users.noreply.github.com>
2018-09-05 22:08:36 -0700

    Move chmod outside for loop.

--- makewavs.sh
+++ makewavs.sh
@@ -3,8 +3,8 @@
 # assumes 22050 Hz, 2 ch, mono because all the sounds
 # in the original iPack are that format.
 # You can check the unpacker output to confirm.
+chmod -R +rw *
 for file in */*.wav; do
-    chmod -R +rw *
     mv "$file" "${file%.wav}.pcm"
     ffmpeg -f s16le -ar 22.05k -ac 1 -i "${file%.wav}.pcm" $file
 done
